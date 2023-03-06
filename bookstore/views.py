bb26406abf617de5c727b6f46f47c1dee3e48e49
anna_joachimska <ajoachimska@bee2code.com>
2023-03-06 11:18:55 +0100

    fixes in detail and AddOrDelete views

--- bookstore/views.py
+++ bookstore/views.py
@@ -4,17 +4,17 @@ from rest_framework.response import Response
 from rest_framework import serializers, generics
 from rest_framework import status
 
-class BookstoresView(generics.GenericAPIView):
 
+class BookstoresView(generics.GenericAPIView):
     serializer_class = BookstoreSerializer
 
     def get(self, request):
         bookstores = Bookstore.objects.all().values()
         total_bookstores = bookstores.count()
         return Response({
-                "status":status.HTTP_200_OK,
-                "total": total_bookstores,
-                "data": bookstores,
+            "status": status.HTTP_200_OK,
+            "total": total_bookstores,
+            "data": bookstores,
         })
 
     def post(self, request):
@@ -28,6 +28,7 @@ class BookstoresView(generics.GenericAPIView):
         else:
             return Response({"status": "fail", "message": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
 
+
 class BookstoreDetail(generics.GenericAPIView):
     queryset = Bookstore.objects.all()
     serializer_class = BookstoreSerializer
@@ -40,35 +41,38 @@ class BookstoreDetail(generics.GenericAPIView):
 
     def get(self, request, pk):
         bookstore = self.get_bookstore(pk=pk)
-        if bookstore == None:
-            return Response({"status": "fail", "message": f"Bookstore with id: {pk} not found"}, status=status.HTTP_404_NOT_FOUND)
+        if bookstore is None:
+            return Response({"status": "fail", "message": f"Bookstore with id: {pk} not found"},
+                            status=status.HTTP_404_NOT_FOUND)
 
         serializer = self.serializer_class(bookstore)
         return Response({"status": "success", "bookstore": serializer.data})
 
     def patch(self, request, pk):
         bookstore = self.get_bookstore(pk)
-        if bookstore == None:
+        if bookstore is None:
             return Response({"status": "fail", "message": f"Bookstore with id: {pk} not found"},
                             status=status.HTTP_404_NOT_FOUND)
 
-        serializer = self.serializer_class(
-            bookstore, data=request.data, partial=True)
-        if bookstore.books.filter(pk=request.data['books'][0]).exists():
-            return Response({"status": "fail", "message": "this book already is in this bookstore"},
-                            status=status.HTTP_400_BAD_REQUEST)
-        elif bookstore.publishing_houses.filter(pk=request.data['publishing_houses'][0]).exists():
-            return Response({"status": "fail", "message": "this publishing house already is in this bookstore"},
-                            status=status.HTTP_400_BAD_REQUEST)
-        else:
+        serializer = self.serializer_class(bookstore, data=request.data, partial=True)
+        try:
+            for publishing_house in request.data['publishing_houses']:
+                if bookstore.publishing_houses.filter(pk=publishing_house).exists():
+                    return Response({"status": "fail", "message": "this publishing house already is in this bookstore"},
+                                    status=status.HTTP_400_BAD_REQUEST)
+        except KeyError:
             if serializer.is_valid():
                 serializer.save()
                 return Response({"status": "success", "bookstore": serializer.data})
             return Response({"status": "fail", "message": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"status": "success", "bookstore": serializer.data})
+        return Response({"status": "fail", "message": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, pk):
         bookstore = self.get_bookstore(pk)
-        if bookstore == None:
+        if bookstore is None:
             return Response({"status": "fail", "message": f"Bookstore with id: {pk} not found"},
                             status=status.HTTP_404_NOT_FOUND)
 
@@ -76,68 +80,7 @@ class BookstoreDetail(generics.GenericAPIView):
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-class BookstoreBooks(generics.GenericAPIView):
-    queryset = Bookstore.objects.all()
-    serializer_class = BookstoreSerializer
-
-
-    def get_bookstore(self, pk):
-        try:
-            return Bookstore.objects.get(pk=pk)
-        except:
-            return None
-
-    def get(self, request, pk):
-        bookstore = self.get_bookstore(pk=pk)
-        if bookstore == None:
-            return Response({"status": "fail", "message": f"Bookstore with id: {pk} not found"}, status=status.HTTP_404_NOT_FOUND)
-
-        serializer = self.serializer_class(bookstore)
-        return Response({"status": "success", "bookstore": serializer.data})
-
-
-    def post(self, request, pk):
-        bookstore = self.get_bookstore(pk)
-        if bookstore == None:
-            return Response({"status": "fail", "message": f"Bookstore with id: {pk} not found"},
-                            status=status.HTTP_404_NOT_FOUND)
-
-        serializer = self.serializer_class(
-            bookstore, data=request.data, partial=True)
-
-        for i in range(0, len(request.data['books'])):
-            if bookstore.books.filter(pk=request.data['books'][i]).exists():
-
-                return Response({"status": "fail", "message": "this book already is in this bookstore"},
-                                status=status.HTTP_400_BAD_REQUEST)
-            else:
-                bookstore.books.add(request.data['books'][i])
-        if serializer.is_valid():
-            return Response(
-                {"status": "success", "message": "book added succesfully", "bookstore": serializer.data})
-        return Response({"status": "fail", "message": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk):
-        bookstore = self.get_bookstore(pk)
-        if bookstore == None:
-            return Response({"status": "fail", "message": f"Bookstore with id: {pk} not found"},
-                            status=status.HTTP_404_NOT_FOUND)
-
-        serializer = self.serializer_class(
-            bookstore, data=request.data, partial=True)
-
-        for i in range(0, len(request.data['books'])):
-            if not bookstore.books.filter(pk=request.data['books'][i]).exists():
-                return Response({"status": "fail", "message": "this book isn't in this bookstore"},
-                                status=status.HTTP_400_BAD_REQUEST)
-            else:
-                bookstore.books.remove(request.data['books'][i])
-        if serializer.is_valid():
-            return Response(
-                {"status": "success", "message": "books deleted succesfully", "bookstore": serializer.data})
-        return Response({"status": "fail", "message": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
-
-class BookstorePublishingHouses(generics.GenericAPIView):
+class AddOrDeletePublishingHouseFromBookstore(generics.GenericAPIView):
     queryset = Bookstore.objects.all()
     serializer_class = BookstoreSerializer
 
@@ -149,50 +92,66 @@ class BookstorePublishingHouses(generics.GenericAPIView):
 
     def get(self, request, pk):
         bookstore = self.get_bookstore(pk=pk)
-        if bookstore == None:
-            return Response({"status": "fail", "message": f"Bookstore with id: {pk} not found"}, status=status.HTTP_404_NOT_FOUND)
+        if bookstore is None:
+            return Response({"status": "fail", "message": f"Bookstore with id: {pk} not found"},
+                            status=status.HTTP_404_NOT_FOUND)
 
         serializer = self.serializer_class(bookstore)
         return Response({"status": "success", "bookstore": serializer.data})
 
-
     def post(self, request, pk):
         bookstore = self.get_bookstore(pk)
-        if bookstore == None:
+        if bookstore is None:
             return Response({"status": "fail", "message": f"Bookstore with id: {pk} not found"},
                             status=status.HTTP_404_NOT_FOUND)
 
-        serializer = self.serializer_class(
-            bookstore, data=request.data, partial=True)
+        serializer = self.serializer_class(bookstore, data=request.data, partial=True)
 
-        for i in range(0, len(request.data['publishing_houses'])):
-            if bookstore.publishing_houses.filter(pk=request.data['publishing_houses'][i]).exists():
+        if serializer.is_valid():
 
-                return Response({"status": "fail", "message": "this book already is in this bookstore"},
+            new_publishing_houses_list = []
+            old_publishing_houses_list = []
+            for publishing_house in request.data['publishing_houses']:
+                if bookstore.publishing_houses.filter(pk=publishing_house).exists():
+                    old_publishing_houses_list.append(publishing_house)
+                else:
+                    new_publishing_houses_list.append(publishing_house)
+            if len(old_publishing_houses_list) > 0:
+                return Response({"status": "fail", "message": "this publishing house already is in this bookstore"},
                                 status=status.HTTP_400_BAD_REQUEST)
             else:
-                bookstore.publishing_houses.add(request.data['publishing_houses'][i])
-        if serializer.is_valid():
+                for publishing_house in new_publishing_houses_list:
+                    bookstore.publishing_houses.add(publishing_house)
+
             return Response(
                 {"status": "success", "message": "publishing house added succesfully", "bookstore": serializer.data})
+
         return Response({"status": "fail", "message": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, pk):
         bookstore = self.get_bookstore(pk)
-        if bookstore == None:
+        if bookstore is None:
             return Response({"status": "fail", "message": f"Bookstore with id: {pk} not found"},
                             status=status.HTTP_404_NOT_FOUND)
 
-        serializer = self.serializer_class(
-            bookstore, data=request.data, partial=True)
+        serializer = self.serializer_class(bookstore, data=request.data, partial=True)
 
-        for i in range(0, len(request.data['publishing_houses'])):
-            if not bookstore.publishing_houses.filter(pk=request.data['publishing_houses'][i]).exists():
+        if serializer.is_valid():
+            new_publishing_house_list = []
+            old_publishing_house_list = []
+            for publishing_house in request.data['publishing_houses']:
+                if not bookstore.publishing_houses.filter(pk=publishing_house).exists():
+                    old_publishing_house_list.append(publishing_house)
+                else:
+                    new_publishing_house_list.append(publishing_house)
+
+            if len(old_publishing_house_list) > 0:
                 return Response({"status": "fail", "message": "this publishing house isn't in this bookstore"},
                                 status=status.HTTP_400_BAD_REQUEST)
             else:
-                bookstore.publishing_house.remove(request.data['publishing_house'][i])
-        if serializer.is_valid():
+                for publishing_house in new_publishing_house_list:
+                    bookstore.publishing_houses.remove(publishing_house)
+
             return Response(
                 {"status": "success", "message": "publishing house deleted succesfully", "bookstore": serializer.data})
         return Response({"status": "fail", "message": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
--- publishing_house/views.py
+++ publishing_house/views.py
@@ -4,17 +4,17 @@ from rest_framework.response import Response
 from rest_framework import serializers, generics
 from rest_framework import status
 
-class PublishingHousesView(generics.GenericAPIView):
 
+class PublishingHousesView(generics.GenericAPIView):
     serializer_class = PublishingHouseSerializer
 
     def get(self, request):
         publishing_houses = PublishingHouse.objects.all().values()
         total_publishing_houses = publishing_houses.count()
         return Response({
-                "status":status.HTTP_200_OK,
-                "total": total_publishing_houses,
-                "data": publishing_houses,
+            "status": status.HTTP_200_OK,
+            "total": total_publishing_houses,
+            "data": publishing_houses,
         })
 
     def post(self, request):
@@ -28,6 +28,7 @@ class PublishingHousesView(generics.GenericAPIView):
         else:
             return Response({"status": "fail", "message": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
 
+
 class PublishingHouseDetail(generics.GenericAPIView):
     queryset = PublishingHouse.objects.all()
     serializer_class = PublishingHouseSerializer
@@ -40,15 +41,16 @@ class PublishingHouseDetail(generics.GenericAPIView):
 
     def get(self, request, pk):
         publishing_house = self.get_publishing_house(pk=pk)
-        if publishing_house == None:
-            return Response({"status": "fail", "message": f"Publishing House with id: {pk} not found"}, status=status.HTTP_404_NOT_FOUND)
+        if publishing_house is None:
+            return Response({"status": "fail", "message": f"Publishing House with id: {pk} not found"},
+                            status=status.HTTP_404_NOT_FOUND)
 
         serializer = self.serializer_class(publishing_house)
         return Response({"status": "success", "publishing house": serializer.data})
 
     def patch(self, request, pk):
         publishing_house = self.get_publishing_house(pk)
-        if publishing_house == None:
+        if publishing_house is None:
             return Response({"status": "fail", "message": f"Publishing House with id: {pk} not found"},
                             status=status.HTTP_404_NOT_FOUND)
 
@@ -62,7 +64,7 @@ class PublishingHouseDetail(generics.GenericAPIView):
     def delete(self, request, pk):
         publishing_house = self.get_publishing_house(pk)
         serializer = self.serializer_class(publishing_house)
-        if publishing_house == None:
+        if publishing_house is None:
             return Response({"status": "fail", "message": f"Publishing House with id: {pk} not found"},
                             status=status.HTTP_404_NOT_FOUND)
         if len(serializer.data['books']) > 0:
@@ -70,65 +72,5 @@ class PublishingHouseDetail(generics.GenericAPIView):
                             status=status.HTTP_400_BAD_REQUEST)
 
         publishing_house.delete()
-        return Response({"status": "success", "message": "publishing house deleted succesfully"},status=status.HTTP_204_NO_CONTENT)
-
-class PublishingHouseBooks(generics.GenericAPIView):
-    queryset = PublishingHouse.objects.all()
-    serializer_class = PublishingHouseSerializer
-
-
-    def get_publishing_house(self, pk):
-        try:
-            return PublishingHouse.objects.get(pk=pk)
-        except:
-            return None
-
-    def get(self, request, pk):
-        publishing_house = self.get_publishing_house(pk=pk)
-        if publishing_house == None:
-            return Response({"status": "fail", "message": f"Publishing House with id: {pk} not found"}, status=status.HTTP_404_NOT_FOUND)
-
-        serializer = self.serializer_class(publishing_house)
-        return Response({"status": "success", "publishing house": serializer.data})
-
-
-    def post(self, request, pk):
-        publishing_house = self.get_publishing_house(pk)
-        if publishing_house == None:
-            return Response({"status": "fail", "message": f"Publishing House with id: {pk} not found"},
-                            status=status.HTTP_404_NOT_FOUND)
-
-        serializer = self.serializer_class(
-            publishing_house, data=request.data, partial=True)
-
-        for i in range(0, len(request.data['books'])):
-            if publishing_house.books.filter(pk=request.data['books'][i]).exists():
-
-                return Response({"status": "fail", "message": "this book already is in this publishing house"},
-                                status=status.HTTP_400_BAD_REQUEST)
-            else:
-                publishing_house.books.add(request.data['books'][i])
-        if serializer.is_valid():
-            return Response(
-                {"status": "success", "message": "book added succesfully", "publishing house": serializer.data})
-        return Response({"status": "fail", "message": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk):
-        publishing_house = self.get_publishing_house(pk)
-        if publishing_house == None:
-            return Response({"status": "fail", "message": f"Publishing House with id: {pk} not found"},
-                            status=status.HTTP_404_NOT_FOUND)
-
-        serializer = self.serializer_class(
-            publishing_house, data=request.data, partial=True)
-
-        for i in range(0, len(request.data['books'])):
-            if not publishing_house.books.filter(pk=request.data['books'][i]).exists():
-                return Response({"status": "fail", "message": "this book isn't in this publishing house"},
-                                status=status.HTTP_400_BAD_REQUEST)
-            else:
-                publishing_house.books.remove(request.data['books'][i])
-        if serializer.is_valid():
-            return Response(
-                {"status": "success", "message": "books deleted succesfully", "publishing house": serializer.data})
-        return Response({"status": "fail", "message": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+        return Response({"status": "success", "message": "publishing house deleted succesfully"},
+                        status=status.HTTP_204_NO_CONTENT)
